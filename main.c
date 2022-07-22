0368a992fb73aa80177f6c915bea18582066d2ab
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-22 00:37:26 -0400

    add elements to gitignore and first line code

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    if (1 > 0) {
+        printf("Primer Hola mundo desde este repo");
+    } else {
+        printf("Algo ha salido, sin embargo --> Hola Mundo, desde Merida");
+    }
+}
