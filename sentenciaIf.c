2f6437d8cbfe552e0e0923352cc9a24410fae96e
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-24 00:12:54 -0400

    add fixes on the code for the PR

--- sentenciaIf.c
+++ sentenciaIf.c
@@ -3,15 +3,18 @@
 int n = 21;
 
 int main() {
-    if (n > 20 && n < 20) {
-        printf("El numero es mayor que diez, pero es menor que 20");
-    } else if (n == 10){
-        printf("El numero es diez");
-    } else if (n > 20) {
-        printf("El numero es mayor que 20");
-    } else if (n < 10){
-        printf("El numero es menor que 10");
-    }
+    if (n > 20 && n < 20)
+    printf("El numero es mayor que diez, pero es menor que 20");
+
+    else if (n == 10)
+    printf("El numero es diez");
+
+    else if (n > 20) 
+    printf("El numero es mayor que 20");
+
+    else if (n < 10)
+    printf("El numero es menor que 10");
+    
     else 
     printf("Error no hay numero");
 }
